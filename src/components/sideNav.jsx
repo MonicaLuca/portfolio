32661d16ac5ea69dadf8fd5f7450b87428d9e887
MonicaLuca <monica.lucarini28@gmail.com>
2020-10-21 15:31:37 -0400

    styled side navbar with working social buttons

--- src/components/sideNav.jsx
+++ src/components/sideNav.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import '../styles/sideNav.css'
+import IconButton from '@material-ui/core/IconButton';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import CreateIcon from '@material-ui/icons/Create';
 
 export default class SideNav extends Component {
   render() {
@@ -10,13 +14,26 @@ export default class SideNav extends Component {
         <h2 className='nav_name'> Monica Lucarini </h2>
         <h3 className='nav_email'>monica.lucarini28@gmail.com</h3>
         </div>
-        <button>Introduction</button>
-        <button>About</button>
-        <button>Skills</button>
-        <button>My TimeLine</button>
-        <button>Projects</button>
-        <button>My Resume</button>
+        <div className="nav_button_group">
+        <a href="#" className="nav_button">Introduction</a>
+        <a href="#" className="nav_button">About</a>
+        <a href="#" className="nav_button">Projects</a>
+        <a href="#" className="nav_button">My Timeline</a>
+        </div>
+        <div className="icon_button_group">
+        <IconButton href='https://www.linkedin.com/in/monica-lucarini/' aria-label="LinkedIn Link">
+        <LinkedInIcon fontSize="large"></LinkedInIcon>
+        </IconButton>
+        <IconButton className="social_icon" href='https://github.com/MonicaLuca' aria-label="Github Link">
+        <GitHubIcon fontSize="large"></GitHubIcon>
+        </IconButton>
+        <IconButton className="social_icon" href='https://medium.com/@monica.lucarini28' aria-label="Medium (blog) Link">
+        <CreateIcon fontSize="large"></CreateIcon>
+        </IconButton>
+        </div>
       </div>
+
+      
     )
   }
 }
