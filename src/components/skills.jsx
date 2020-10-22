3032e02bfbc44a1ca18831ed533c6142066a5ae5
MonicaLuca <monica.lucarini28@gmail.com>
2020-10-21 22:23:02 -0400

    created and styled languages  with hover effect

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -32,6 +32,29 @@ export default class Skills extends Component {
        </div>
        </div>
      </div>
+      <div>
+       <h3 className='skills_subtitle'>Languages</h3>
+       <div className='back_end'>
+         <div class="skill_container">
+       <img className='img_skills'src="https://miro.medium.com/max/1052/1*wF-Gv5GJep4Xszk9_zzJng.jpeg" alt="JavaScript" height='180px' width="180px"></img>
+        <div className='skill_name_position'>
+          <div className='skill_name'> JavaScript</div>
+        </div>
+       </div>
+       <div class="skill_container">
+       <img className='img_skills'src="https://i.dlpng.com/static/png/192335_preview.png" alt="Ruby" height='180px' width="180px"></img>
+        <div className='skill_name_position'>
+          <div className='skill_name'>Ruby</div>
+        </div>
+       </div>
+       <div class="skill_container">
+       <img className='img_skills'src="https://thumbs.dreamstime.com/b/sql-database-icon-logo-design-ui-ux-app-orange-inscription-shadow-96841969.jpg" alt="SQL" height='180px' width="180px"></img>
+        <div className='skill_name_position'>
+          <div className='skill_name'>SQL</div>
+        </div>
+       </div>
+       </div>
+     </div>
     </div>
   )
 
