03a701f49d0b78ee40cd2c2215858d47499ea59d
MonicaLuca <monica.lucarini28@gmail.com>
2020-10-25 18:18:05 -0400

    created projects nav component with buttons

---
+++ src/components/projectsNav.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/projectsNav.css'
+
+export default class ProjectsNav extends Component {
+  render() {
+    return (  
+    <div>
+      <nav className='projects_nav'>
+        <Link className="projects_button" to="/">Kitchionary</Link>
+        <Link className="projects_button" to="/GoodBuy">GoodBuy</Link>
+        <Link className="projects_button" to="/Jungle">Jungle</Link>
+        <Link className="projects_button" to="/Scheduler">Scheduler</Link>
+        <Link className="projects_button" to="/TinyApp">Tiny App</Link>
+      </nav>
+    </div>
+  )
+
+  }
+}
